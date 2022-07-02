1ab512af62b73988b22e49b6ee912b7d9b0b993c
caihaochen <15955142089@163.com>
2022-07-02 13:37:34 +0800

    提交框架代码

---
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -0,0 +1,26 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * 类操作工具类
+ */
+public final class ClassUtil {
+    // 日志
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+
+    public static ClassLoader getClassLoader() {
+
+    }
+
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+
+    }
+
+    public static Set<Class<?>> getClassSet(String packageName) {
+
+    }
+}
