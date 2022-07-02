f9bc51a64904155045439263618f29db0cdde7a7
caihaochen <15955142089@163.com>
2022-07-02 21:33:42 +0800

    【message】提交ControllerHelper，封装了一个Action Map，通过Map来存放Request和Handler映射关系，通过ClassHelper获取所有带有Controller注解的类，遍历这些Controller类，从Action提取URL，初始化Request和Handler之间的映射关系

---
+++ src/main/java/org/smart4j/framework/bean/Handler.java
@@ -0,0 +1,39 @@
+package org.smart4j.framework.bean;
+
+import java.lang.reflect.Method;
+
+/**
+ * 封装 Action 信息
+ */
+public class Handler {
+    /**
+     * Controller类
+     */
+    private Class<?> controllerClass;
+
+    /**
+     * Action 方法
+     */
+    private Method actionMethod;
+
+    public Handler(Class<?> controllerClass, Method actionMethod) {
+        this.controllerClass = controllerClass;
+        this.actionMethod = actionMethod;
+    }
+
+    public Class<?> getControllerClass() {
+        return controllerClass;
+    }
+
+    public void setControllerClass(Class<?> controllerClass) {
+        this.controllerClass = controllerClass;
+    }
+
+    public Method getActionMethod() {
+        return actionMethod;
+    }
+
+    public void setActionMethod(Method actionMethod) {
+        this.actionMethod = actionMethod;
+    }
+}
